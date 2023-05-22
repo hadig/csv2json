f5d736827a9515e1f8b995ad453f68bbecf16938
hadig <4592275+hadig@users.noreply.github.com>
2023-05-22 20:17:10 +0200

    Support negative numbers

--- Sources/Csv2json.swift
+++ Sources/Csv2json.swift
@@ -111,7 +111,7 @@ struct RuntimeError: Error, CustomStringConvertible {
 extension String {
     var isNumber: Bool {
         return self.range(
-            of: "^[0-9,.]*$", // 1
+            of: "^[0-9,.-]*$",
             options: .regularExpression) != nil
     }
 }
